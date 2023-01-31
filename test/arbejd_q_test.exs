c14e4e92d980d13ba058a8ba6c52ba9c4160b57e
Paul Fleischer <pf@qarmainspect.com>
2023-01-31 11:57:02 +0100

    Reformat file

--- test/arbejd_q_test.exs
+++ test/arbejd_q_test.exs
@@ -5,19 +5,21 @@ defmodule ArbejdQTest do
   alias ArbejdQ.Execution
 
   test "Enqueue job", _tags do
-    assert {:ok, job} = ArbejdQ.enqueue_job("default", ArbejdQ.Test.Worker,
-                                            %{
-                                              duration: 1
-                                            })
+    assert {:ok, job} =
+             ArbejdQ.enqueue_job("default", ArbejdQ.Test.Worker, %{
+               duration: 1
+             })
+
     [queued_job] = ArbejdQ.list_queued_jobs("default")
     assert job.id == queued_job.id
 
     parent = self()
-    spawn_link fn ->
+
+    spawn_link(fn ->
       Ecto.Adapters.SQL.Sandbox.allow(ArbejdQ.Test.Repo, parent, self())
       {:ok, job} = Execution.take_job(queued_job)
       Execution.execute_job(job)
-    end
+    end)
 
     assert {:ok, :done, {:ok, 1}} == ArbejdQ.wait(job.id)
 
@@ -27,39 +29,44 @@ defmodule ArbejdQTest do
   end
 
   test "Concurrent execution of job" do
-    assert {:ok, job} = ArbejdQ.enqueue_job("default", ArbejdQ.Test.Worker,
-                                            %{
-                                              duration: 1
-                                            })
+    assert {:ok, job} =
+             ArbejdQ.enqueue_job("default", ArbejdQ.Test.Worker, %{
+               duration: 1
+             })
 
     parent = self()
-    spawn_link fn ->
+
+    spawn_link(fn ->
       Ecto.Adapters.SQL.Sandbox.allow(ArbejdQ.Test.Repo, parent, self())
+
       result =
         case Execution.take_job(job) do
           {:ok, job} ->
             {:ok, _, res} = Execution.execute_job(job)
             res
 
-          other -> other
+          other ->
+            other
         end
 
-      send parent, {:job_result, result}
-    end
+      send(parent, {:job_result, result})
+    end)
 
-    spawn_link fn ->
+    spawn_link(fn ->
       Ecto.Adapters.SQL.Sandbox.allow(ArbejdQ.Test.Repo, parent, self())
+
       result =
         case Execution.take_job(job) do
           {:ok, job} ->
             {:ok, _, res} = Execution.execute_job(job)
             res
 
-          other -> other
+          other ->
+            other
         end
 
-      send parent, {:job_result, result}
-    end
+      send(parent, {:job_result, result})
+    end)
 
     assert {:ok, :done, {:ok, 1}} == ArbejdQ.wait(job.id)
 
@@ -78,16 +85,18 @@ defmodule ArbejdQTest do
   end
 
   test "Stale job detection", _tags do
-    assert {:ok, job} = ArbejdQ.enqueue_job("default", ArbejdQ.Test.Worker,
-                                            %{
-                                              duration: 20
-                                            })
+    assert {:ok, job} =
+             ArbejdQ.enqueue_job("default", ArbejdQ.Test.Worker, %{
+               duration: 20
+             })
+
     parent = self()
-    spawn_link fn ->
+
+    spawn_link(fn ->
       Ecto.Adapters.SQL.Sandbox.allow(ArbejdQ.Test.Repo, parent, self())
       {:ok, job} = Execution.take_job(job)
       {:ok, _job, _result} = Execution.execute_job(job)
-    end
+    end)
 
     Process.sleep(1_000)
     assert [] = ArbejdQ.list_stale_jobs("default")
